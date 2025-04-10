7a78b3b02831a9710bd5f52fd2e067ae5afb250e
Michael Calkins <calkinm@amazon.com>
2025-04-09 19:14:48 -0700

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,30 @@
-"use client";
+'use client';
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
+import { useState, useEffect } from 'react';
+import { generateClient } from 'aws-amplify/data';
+import type { Schema } from '@/amplify/data/resource';
+import './../app/app.css';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import ButtonListPoc from '@/src/ui-components/ButtonListPoc';
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+  const items = [
+    { id: '1', label: 'Button 1' },
+    { id: '2', label: 'Button 2' },
+    { id: '3', label: 'Button 3' },
+  ];
 
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+      buttons
+      <ButtonListPoc items={items} />
+      after
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
